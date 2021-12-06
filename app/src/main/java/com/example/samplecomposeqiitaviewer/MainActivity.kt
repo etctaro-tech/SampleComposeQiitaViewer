66aa33898e2fc97bb94cf0c18b5d29f125e460bd
Tobe Yutaro <tobe.yutaro@ntt-tx.co.jp>
2021-12-06 19:22:04 +0900

    2ペインで2コンポーザブル間の情報のやりとりを一応実現

--- app/src/main/java/com/example/samplecomposeqiitaviewer/MainActivity.kt
+++ app/src/main/java/com/example/samplecomposeqiitaviewer/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.example.samplecomposeqiitaviewer
 
-import android.content.Context
-import android.content.Intent
-import android.content.Intent.ACTION_VIEW
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
@@ -19,14 +19,14 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.content.ContextCompat.startActivity
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.samplecomposeqiitaviewer.entity.QiitaArticle
 import com.example.samplecomposeqiitaviewer.nw.QiitaApi
 import com.example.samplecomposeqiitaviewer.ui.SampleComposeQiitaViewerTheme
@@ -49,55 +49,82 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun HomeScreen() {
-
     val qiitaArticles = remember { mutableStateListOf<QiitaArticle>() }
+    val selectedArticle = remember { mutableStateOf<QiitaArticle?>(null) }
 
     MaterialTheme {
         Scaffold(
-            content = {
-                QiitaItemList(items = qiitaArticles)
-            },
-            floatingActionButton = {
-                ExtendedFloatingActionButton(
-                    onClick = {
-                        MainScope().launch {
-                            try {
-                                val articles = QiitaApi().getAllArticles()
-                                Log.d("QiitaViewer", "FAB: $articles")
-                                qiitaArticles.clear()
-                                qiitaArticles.addAll(articles)
 
-                            } catch (e: Exception) {
-                                Log.e("QiitaViewer", "FAB: Exception", e)
+        ) {
+            Row {
+                Box(modifier = Modifier.weight(1.0F)) {
+                    ListScreen(
+                        items = qiitaArticles,
+                        onItemsChanged = {
+                            MainScope().launch {
+                                try {
+                                    val articles = QiitaApi().getAllArticles()
+                                    Log.d("QiitaViewer", "FAB: $articles")
+                                    qiitaArticles.clear()
+                                    qiitaArticles.addAll(articles)
+
+                                } catch (e: Exception) {
+                                    Log.e("QiitaViewer", "FAB: Exception", e)
+                                }
                             }
-                        }
-                    },
-                    text = { Text("reload") },
-                    icon = { Icon(imageVector = Icons.Filled.Refresh, contentDescription = null) },
-                    modifier = Modifier.semantics { contentDescription = "Refresh Button" }
-                )
+                        },
+                        onItemSelected = {
+                            selectedArticle.value = it }
+                    )
+                }
+                Box(modifier = Modifier.weight(1.0F)) {
+                    DetailScreen(url = selectedArticle.value?.url ?: "")
+                }
             }
-        )
+        }
     }
+}
+
+@Composable
+fun ListScreen(items: List<QiitaArticle>, onItemsChanged:()-> Unit, onItemSelected : (QiitaArticle)->Unit) {
+    Scaffold(
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = onItemsChanged,
+                text = { Text("reload") },
+                icon = { Icon(imageVector = Icons.Filled.Refresh, contentDescription = null) },
+                modifier = Modifier.semantics { contentDescription = "Refresh Button" }
+            )
+        }
+    ) {
+        QiitaItemList(items = items, onItemSelected = onItemSelected)
+    }
+}
+
+@Composable
+fun DetailScreen(url: String) {
+    if (url.isEmpty()) {
+        Text("ここに選んだページが表示されるイメージ")
+    } else {
+        MyWebComposable(url)
+    }
+}
+
+@Composable
+private fun MyWebComposable(url: String) {
+    AndroidView(factory = { ctx ->
+        WebView(ctx)
+    }, update = {
+        it.webViewClient = WebViewClient()
+        it.loadUrl(url)
+    }, modifier = Modifier.fillMaxSize()
+    )
 }
 
 @Preview
 @Composable
-fun PrevHomeScreen() {
-    MaterialTheme {
-        Scaffold(
-            content = {
-                QiitaItemList(items = listOf())
-            },
-            floatingActionButton = {
-                ExtendedFloatingActionButton(
-                    onClick = {},
-                    text = { Text("reload") },
-                    icon = { Icon(imageVector = Icons.Filled.Refresh, contentDescription = null) }
-                )
-            }
-        )
-    }
+fun PrevDetailScreen() {
+    DetailScreen(url = "http://google.co.jp/")
 }
 
 @Preview(showBackground = true)
@@ -109,14 +136,9 @@ fun DefaultPreview() {
 }
 
 @Composable
-fun QiitaItem(title: String, url: String) {
-    val context: Context = LocalContext.current
-
-    Row(Modifier.clickable(onClick = {
-        val uri = Uri.parse(url)
-        val intent = Intent(ACTION_VIEW, uri)
-        startActivity(context, intent, null)
-    })) {
+fun QiitaItem(title: String, url: String, modifier: Modifier) {
+    Row(modifier = modifier)
+    {
         Column {
             Text(text = title, style = typography.h6)
             Text(text = url, style = typography.body2)
@@ -125,11 +147,12 @@ fun QiitaItem(title: String, url: String) {
 }
 
 @Composable
-fun QiitaItemList(items: List<QiitaArticle>) {
+fun QiitaItemList(items: List<QiitaArticle>, onItemSelected: (QiitaArticle) -> Unit) {
     LazyColumn(
         modifier = Modifier.semantics { contentDescription = "Item List" }) {
         items(items) { item ->
-            QiitaItem(title = item.title, url = item.url)
+            QiitaItem(title = item.title, url = item.url, modifier = Modifier.clickable(onClick = {
+                onItemSelected(item) }))
             Divider(color = Color.Black)
         }
     }
@@ -139,17 +162,24 @@ fun QiitaItemList(items: List<QiitaArticle>) {
 @Preview
 @Composable
 fun prevQiitaItem() {
-    QiitaItem("title1", "http://sample.co.jp/")
+    QiitaItem("title1", "http://sample.co.jp/", modifier = Modifier)
 }
+
+
 
 @Preview
 @Composable
-fun prevQiitaItemList() {
-    QiitaItemList(
-        listOf(
-            QiitaArticle("001", "title1", "http://sample.co.jp/1"),
-            QiitaArticle("002", "title2", "http://sample.co.jp/2"),
-            QiitaArticle("003", "title3", "http://sample.co.jp/3"),
-        )
-    )
+fun MyWebPrev() {
+    val weburl = remember { mutableStateOf("")}
+    Column {
+        Row {
+            Button(onClick = { weburl.value = "http://google.co.jp/" }) {
+               Text("Url1")
+            }
+            Button(onClick = { weburl.value = "http://yahoo.co.jp/" }) {
+                Text("Url2")
+            }
+        }
+        MyWebComposable(url = weburl.value)
+    }
 }
